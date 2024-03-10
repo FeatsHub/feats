636dc92380f7ed0bb5dd61fa1328e999482f3f76
Manuel Valverde <manuelvalverde.dev@gmail.com>
2024-03-10 13:54:44 +0100

    ssl config gunicorn

---
+++ backend/gunicorn_config.py
@@ -0,0 +1,29 @@
+import os
+
+bind = "0.0.0.0:8000"
+workers = int(os.environ.get("UWSGI_PROCESSES", "4"))
+threads = int(os.environ.get("UWSGI_THREADS", "2"))
+worker_class = "sync"
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+
+# Allow Range requests
+limit_request_line = 8190
+
+# Processes and threads
+preload_app = True
+chdir = "/app"
+module = "main.wsgi:application"
+
+# Better startup/shutdown in docker:
+graceful_timeout = 10
+timeout = 600
+
+# Avoid errors on aborted client connections
+ignore_sigpipe = True
+post_buffering = 4096
+
+certfile = "/etc/letsencrypt/live/api.feats-api.com/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/api.feats-api.com/privkey.pem"
--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-bind = "0.0.0.0:8000"
-module = "main.wsgi:application"
-
-workers = 1
-worker_connections = 1000
-threads = 4
-
-certfile = "/etc/letsencrypt/live/api.feats-api.com/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/api.feats-api.com/privkey.pem"
