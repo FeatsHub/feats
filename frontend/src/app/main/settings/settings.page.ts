de09fc6842d3634f01226a284561c3484a4e4ee8
Manuel Valverde <manuelvalverde.dev@gmail.com>
2024-04-02 19:23:36 +0200

    use manual darkmode

--- frontend/src/app/main/settings/settings.page.ts
+++ frontend/src/app/main/settings/settings.page.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SettingsPage implements OnInit {
 
   selectedAllergens = [1, 3, 5]
+  themeToggle = false;
 
   settings = [
     {
@@ -21,6 +22,28 @@ export class SettingsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark theme based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
+  }
+
+  initializeDarkTheme(isDark: boolean) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
+  }
+
+  toggleChange(ev: any) {
+    this.toggleDarkTheme(ev.detail.checked);
+  }
+
+  // Add or remove the "dark" class on the document body
+  toggleDarkTheme(shouldAdd: any) {
+    document.body.classList.toggle('dark', shouldAdd);
   }
 
 }
